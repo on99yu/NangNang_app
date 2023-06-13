c609c003ccc96b253804e4d6141a15429cc060f7
hyuk9833 <hyuk9833@naver.com>
2023-06-13 17:52:33 +0900

    token balance check

--- app.js
+++ app.js
@@ -37,9 +37,9 @@ app.post('/transaction', async (req,res)=>{
 
 app.post('/getBalance', async(req,res)=>{
     const data = req.body;
-    var usdtBalance = await getBalance(data.tokenName,data.walletAddress);
-    console.log("가격은",usdtBalance);
-    res.send({"balance" : usdtBalance});
+    var tokenBalance = await getBalance(data.tokenName,data.walletAddress);
+    console.log("가격은",tokenBalance);
+    res.send({"balance" : tokenBalance});
     //res.send(usdtBalance);
 });
 
@@ -68,10 +68,10 @@ async function getTransaction(walletAddress,apiKey){
 }
 
 async function getBalance(tokenName, walletAddress){
-  var contractAddress, data, balanceData;
-  if(tokenName=='USDT'){
-    contractAddress = '0xC2C527C0CACF457746Bd31B2a698Fe89de2b6d49';
-    data = await axios.get(`https://api-goerli.etherscan.io/api?module=account&action=tokenbalance&contractaddress=${contractAddress}&address=${walletAddress}&tag=latest&apikey=${process.env.ETH_SCAN_APIKEY}`).catch(error => {
+  var contractAddress, data, balanceData, tokenDemicalNum;
+  if(tokenName='ETH'){
+    contractAddress = '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6';
+    data = await axios.get(`https://api-goerli.etherscan.io/api?module=account&action=balance&address=${walletAddress}&tag=latest&apikey=${process.env.ETH_SCAN_APIKEY}`).catch(error => {
       if (error.response) {
         // 요청이 이루어졌으며 서버가 2xx의 범위를 벗어나는 상태 코드로 응답했습니다.
         console.log(error.response.data);
@@ -87,11 +87,41 @@ async function getBalance(tokenName, walletAddress){
         console.log('Error', error.message);
       }
       console.log(error.config);
-    }); 
-    balanceData = data.data.result/10**6;
-    //console.log(data);
+    });
+    balanceData = data.data.result/10**18;
+    return balanceData;
+  }else if(tokenName=='USDT'){
+    contractAddress = '0xC2C527C0CACF457746Bd31B2a698Fe89de2b6d49';
+  }else if(tokenName=='USDC'){
+    contractAddress = '0x07865c6E87B9F70255377e024ace6630C1Eaa37F';
+  }else if(tokenName=='UNI'){
+    contractAddress = '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984';
+  }else if(tokenName=='WETH'){
+    contractAddress = '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6'
   }
-  //console.log("가격은 ",balanceData);
+  data = await axios.get(`https://api-goerli.etherscan.io/api?module=account&action=tokenbalance&contractaddress=${contractAddress}&address=${walletAddress}&tag=latest&apikey=${process.env.ETH_SCAN_APIKEY}`).catch(error => {
+    if (error.response) {
+      // 요청이 이루어졌으며 서버가 2xx의 범위를 벗어나는 상태 코드로 응답했습니다.
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // 요청이 이루어 졌으나 응답을 받지 못했습니다.
+      // `error.request`는 브라우저의 XMLHttpRequest 인스턴스 또는
+      // Node.js의 http.ClientRequest 인스턴스입니다.
+      console.log(error.request);
+    } else {
+      // 오류를 발생시킨 요청을 설정하는 중에 문제가 발생했습니다.
+      console.log('Error', error.message);
+    }
+    console.log(error.config);
+  }); 
+  if(tokenName=='USDT'||tokenName=='USDC'){
+    tokenDemicalNum=10**6;
+  }else if(tokenName=='UNI'||tokenName=='WETH'){
+    tokenDemicalNum=10**18;
+  }
+  balanceData = data.data.result/tokenDemicalNum
   return balanceData; 
 }
 app.listen(port,()=>{ 
