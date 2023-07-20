b9294b5a4f5531c4ea7acef3672e2e23b4f93e12
Park yoon <parkyoon97@gmail.com>
2023-07-20 22:51:36 +0900

    useContext Update

--- web/managerseller/src/App.js
+++ web/managerseller/src/App.js
@@ -1,39 +1,23 @@
-import MainHomePage from './pages/MainLandingPage';
-import MainBlockchainManagePage from './pages/MainBlockchainManagePage';
-import MainPaymentRecordsPage from './pages/MainPaymentRecordsPage';
-import MainMyInfoPage from './pages/MainMyInfoPage';
-import MainWalletViewPage from './pages/MainWalletViewPage';
-import MainEmptyPage from './pages/MainEmptyPage';
-import SignInFormPage from './pages/SignInFormPage';
-import SignUpFormPage from './pages/SignUpFormPage';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import UserContext from './contexts/user-context';
-import React, { useState } from 'react';
+import MainHomePage from "./pages/MainLandingPage";
+import MainBlockchainManagePage from "./pages/MainBlockchainManagePage";
+import MainPaymentRecordsPage from "./pages/MainPaymentRecordsPage";
+import MainMyInfoPage from "./pages/MainMyInfoPage";
+import MainWalletViewPage from "./pages/MainWalletViewPage";
+import MainEmptyPage from "./pages/MainEmptyPage";
+import SignInFormPage from "./pages/SignInFormPage";
+import SignUpFormPage from "./pages/SignUpFormPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AuthProvider from "./contexts/AuthContext";
 
 export default function App() {
-  const [user, setUser] = useState({
-    consumer_or_not: undefined,
-    email: '',
-    id: '',
-    phone_number: '',
-    real_name: '',
-    resident_registration_number: '',
-  });
-  const [count, setCount] = useState('');
-
-  const updateUser = (newUser) => {
-    setUser(newUser);
-  };
-
   return (
     <div className="App">
-      <BrowserRouter>
-        <UserContext.Provider value={{ user, updateUser, count, setCount }}>
+      <AuthProvider>
+        <BrowserRouter>
           <Routes>
             <Route path="/" element={<SignInFormPage />} />
             <Route path="/signup" element={<SignUpFormPage />} />
             <Route path="/main" element={<MainHomePage />} />
-
             <Route
               path="/BlockchainManage"
               element={<MainBlockchainManagePage />}
@@ -43,8 +27,8 @@ export default function App() {
             <Route path="/MyInfo" element={<MainMyInfoPage />} />
             <Route path="*" element={<MainEmptyPage />} />
           </Routes>
-        </UserContext.Provider>
-      </BrowserRouter>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
--- web/managerseller/src/components/LoginPage/SignIn.js
+++ web/managerseller/src/components/LoginPage/SignIn.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from "react";
 // import { Link } from 'react-router-dom';
-import classes from './SignIn.module.css';
-import { useState, useEffect, useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { LoginReturnData } from '../../databasefunction/LoginReturnDataFunc';
-import UserContext from '../../contexts/user-context';
+import classes from "./SignIn.module.css";
+import { useState, useEffect, useContext, useCallback } from "react";
+import { Link } from "react-router-dom";
+// import { LoginReturnData } from "../../databasefunction/LoginReturnDataFunc";
+import { AuthContext } from "../../contexts/AuthContext";
+import axios from "axios";
 
 const SignIn = () => {
-  const { user, updateUser } = useContext(UserContext);
-
+  const [state, dispatch] = useContext(AuthContext);
   // const { user, updateUser } = useContext('');
-  const [signInId, setSignInId] = useState('');
-  const [signInPw, setSignInPw] = useState('');
+  const [signInId, setSignInId] = useState("");
+  const [signInPw, setSignInPw] = useState("");
   const [signInInvalid, setSignInInvalid] = useState(false);
   const [loading, setLoading] = useState(false);
 
@@ -28,58 +28,74 @@ const SignIn = () => {
     let id = signInId;
     let pw = signInPw;
 
+    setLoading(true);
     try {
-      setLoading(true);
-      let data = await LoginReturnData(id, pw);
-      console.log('data');
-      if (data === -1) {
-        setSignInInvalid(true);
-      } else {
-        const newUser = {
-          ...user,
-          consumer_or_not: data.consumer_or_not,
-          password: data.password,
-          id: data.id,
-          email: data.email,
-          real_name: data.real_name,
-          resident_registration_number: data.resident_registration_number,
-          phone_number: data.phone_number,
-        };
-
-        updateUser(newUser);
-        alert('Login Successful');
+      const res = await axios({
+        method: "POST",
+        url: "https://asia-northeast3-nangnang-b59c0.cloudfunctions.net/api/login/loginreturndata",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: {
+          input_user_id: "gen1001",
+          input_user_pwd: "gen1001",
+        },
+      });
+      console.log(JSON.stringify(res, null, 2));
+      if (Object.keys(res).length === 0) {
+        // 빈 객체인 경우에 대한 처리
+        throw new Error("Empty response data.");
       }
-      if (user.id === '') {
-        alert('데이터가 적용되지 않았습니다.');
-      } else {
-        window.location.href = '/main';
-      }
+      dispatch({
+        type: "USER_LOGIN",
+        payload: true,
+        id: id,
+        // real_name: res.data.real_name,
+      });
     } catch (error) {
       console.log(error);
-      alert('Login failed');
+      alert("Login failed");
       setSignInInvalid(true);
     } finally {
       setLoading(false);
     }
   };
 
+  // const loginHandler = useCallback(async (event) => {
+  //   event.preventDefault(); // 폼 제출 기본 동작 방지
+  //   let id = signInId;
+  //   let pw = signInPw;
+  //   setLoading(true);
+  //   try {
+  //     const response = await fetch({
+  //       method: "POST",
+  //       url: "https://asia-northeast3-nangnang-b59c0.cloudfunctions.net/api/login/loginreturndata",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       data: {
+  //         input_user_id: "gen1001",
+  //         input_user_pwd: "gen1001",
+  //       },
+  //     });
+  //     if (!response.ok) {
+  //       throw new Error("Something went wrong!");
+  //     }
+  //     const data = await response.json();
+  //     console.log(data);
+  //   } catch (error) {
+  //     console.log(error);
+  //     alert("Login failed");
+  //     setSignInInvalid(true);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // }, []);
+
   useEffect(() => {
     setSignInInvalid(false); // 컴포넌트가 마운트될 때 count 상태를 0으로 설정
-
-    // if (signInData) {
-    //   const updatedUserData = {
-    //     id: signInData.id,
-    //     pw: signInData.password,
-    //     email: signInData.email,
-    //     name: signInData.real_name,
-    //     inumber: signInData.resident_registration_number,
-    //     phone: signInData.phone_number,
-    //   };
-    //   updateUser(updatedUserData);
-    //   setSignInData(updatedUserData);
-    // }
-    // console.log(signInData);
-  }, []);
+    console.log(state);
+  }, [state]);
 
   return (
     <div className={classes.login_component}>
@@ -92,7 +108,7 @@ const SignIn = () => {
             <span className={classes.formtext}>아이디&nbsp;&nbsp;</span>
             <input
               className={classes.inputaddress}
-              type={'text'}
+              type={"text"}
               id="username"
               placeholder=""
               required
@@ -104,7 +120,7 @@ const SignIn = () => {
             <span className={classes.formtext}>비밀번호&nbsp;&nbsp;</span>
             <input
               className={classes.inputaddress}
-              type={'password'}
+              type={"password"}
               id="password"
               placeholder="8자 이상"
               required
@@ -122,7 +138,7 @@ const SignIn = () => {
             </button>
           </div>
         </form>
-        <Link to={'/signup'} className={classes.signupbutton_wrap}>
+        <Link to={"/signup"} className={classes.signupbutton_wrap}>
           <button id="signupbutton" className={classes.signupbutton}>
             회원가입
           </button>
