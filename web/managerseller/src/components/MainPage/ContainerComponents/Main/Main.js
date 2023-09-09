b33c30535cb03a18a4079de22a030161af9045b1
Park yoon <parkyoon97@gmail.com>
2023-09-09 17:22:41 +0900

    useContext 삽입 및 로그인 검증

--- web/managerseller/src/components/MainPage/ContainerComponents/Main/Main.js
+++ web/managerseller/src/components/MainPage/ContainerComponents/Main/Main.js
@@ -1,25 +1,37 @@
 import classes from "./Main.module.css";
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
 import nangnang from "../../../../asset/images/nangnang.png";
+import UserContext from "../../../../contexts/UserContext";
 const Main = () => {
-  // const { user } = useContext(UserContext);
+  const signIn = useContext(UserContext);
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    if (!signIn.isLogin) {
+      navigate('/');
+    }
+  }, [signIn.isLogin, navigate]);
 
   return (
-    <div className={classes.main_wrap}>
-      <div className={classes.main}>
-        {/* <div className={classes.main_text}>Main</div> */}
-        <div className={classes.main_component_wrap}>
-          <div className={classes.main_image_wrap}>
-            <img
-              className={classes.main_image}
-              src={nangnang}
-              alt="낭낭 메인 페이지 이미지"
-            />
-            {/* <p>환영합니다! User: {user ? JSON.stringify(user) : "No user"}</p> */}
+    <>
+      {console.log(signIn)}
+      <div className={classes.main_wrap}>
+        <div className={classes.main}>
+          {/* <div className={classes.main_text}>Main</div> */}
+          <div className={classes.main_component_wrap}>
+            <div className={classes.main_image_wrap}>
+              <img
+                className={classes.main_image}
+                src={nangnang}
+                alt="낭낭 메인 페이지 이미지"
+              />
+              <h1>{signIn.real_name}님 환영합니다!</h1>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
